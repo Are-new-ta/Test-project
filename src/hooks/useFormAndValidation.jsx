b8dac6538934035ae10655ce96f0042706548865
unknown <aptmos2016@yandex.ru>
2023-07-04 20:52:51 +0300

    pull 2

--- src/hooks/useFormAndValidation.jsx
+++ src/hooks/useFormAndValidation.jsx
@@ -7,23 +7,18 @@ export function useFormAndValidation() {
   const [error, setError] = useState({});
   const [isValid, setIsValid] = useState(true);
 
-  // Обработчик изменения инпута обновляет стейт
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setValues({ ...values, [name]: value });
-    setError({ ...error, [name]: e.target.validationMessage });
-    setIsValid(e.target.closest('form').checkValidity());
-  }
 
-  //сброс полей формы и
-  const resetForm = useCallback((
-    updatedValues = {},
-    updatedError = {},
-    updatedIsValid = false) => {
-    setValues(updatedValues);
-    setError(updatedError);
-    setIsValid(updatedIsValid);
-  }, [setValues, setError, setIsValid]);
+}
 
-  return { values, error, isValid, setValues, handleChange, resetForm }
+//сброс полей формы и
+const resetForm = useCallback((
+  updatedValues = {},
+  updatedError = {},
+  updatedIsValid = false) => {
+  setValues(updatedValues);
+  setError(updatedError);
+  setIsValid(updatedIsValid);
+}, [setValues, setError, setIsValid]);
+
+return { values, error, isValid, setValues, handleChange, resetForm }
 }
